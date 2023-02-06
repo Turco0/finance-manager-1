2c7a6cf7f9ed2df25fe6e8d88b24e056e2af76d9
daniellykammers <dany.kammers@hotmail.com>
2023-02-06 18:40:21 -0300

    feat(M01S002): data types and array

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var mesAtual = 'janeiro';
 
+// Array de objetos que representam uma ou mais entradas
+var earningsList = [{ description: 'Salário', value: 4500 }];
+
+// Array de objetos que representam uma ou mais saídas
+var expensesList = [{ description: 'Alimentação', value: 800 }];
+
 // Mês
 document.getElementById('month').innerHTML = mesAtual;
 
@@ -12,8 +18,17 @@ document.getElementById('total-expenses').innerHTML = 1800;
 // Saldo
 document.getElementById('final-balance').innerHTML = 200;
 
-// Minha receita: Salário
-document.getElementById('salary').innerHTML = 2000;
+let earningListAtHtml = document.getElementById('earning-list');
+let expensesListAtHtml = document.getElementById('expense-list');
 
-// Meus gastos: Alimentação
-document.getElementById('food').innerHTML = 200;
+earningsList.forEach((earning) => {
+  let li = document.createElement('li');
+  li.innerText = `${earning.description}: R$ ${earning.value}`;
+  earningListAtHtml.appendChild(li);
+});
+
+expensesList.forEach((expense) => {
+  let li = document.createElement('li');
+  li.innerText = `${expense.description}: R$ ${expense.value}`;
+  expensesListAtHtml.appendChild(li);
+});

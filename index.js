fef753a149606697f546cff08b11bbc519a1d3fe
Iedo <iedo2298@gmail.com>
2023-02-09 19:33:52 -0300

    att

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var saidas = 400;
 var saldo = 1000;
 var salario = 8000;
 var alimentacao = 8000;
+var receita = [{
+    description: 'Salário',
+    value: 4000,
+}]
+var gastos = [{
+    description: 'Alimentação',
+    value: 800,
+}]
 
 // Mês
 document.getElementById('month').innerHTML = mesAtual;
@@ -18,7 +26,7 @@ document.getElementById('total-expenses').innerHTML = saidas;
 document.getElementById('final-balance').innerHTML = saldo;
 
 // Minha receita: Salário
-document.getElementById('salary').innerHTML = salario;
+document.getElementById('salary').innerHTML = `${receita[0].description}: R$${receita[0].value}`;
 
 // Meus gastos: Alimentação
-document.getElementById('food').innerHTML = alimentacao;
+document.getElementById('food').innerHTML = `${gastos[0].description}: R$${gastos[0].value}`;
